7894d05b5cc37f0d5e0a60ebc62ca42f69dc73f8
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-11-13 21:44:48 +0100

    Introducing language chooser as multiplatform DropdownMenu

--- shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchField.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchField.kt
@@ -1,15 +1,18 @@
 package com.faltenreich.rhyme.search
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.faltenreich.rhyme.MR
+import com.faltenreich.rhyme.language.LanguageChooser
 import com.faltenreich.rhyme.shared.localization.getString
 
 @Composable
@@ -19,11 +22,25 @@ fun SearchField(query: String, onValueChange: (String) -> Unit) {
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text(getString(MR.strings.search_placeholder)) },
         trailingIcon = {
-            Icon(
-                Icons.Default.Clear,
-                contentDescription = getString(MR.strings.clear_input),
-                modifier = Modifier.clickable { onValueChange("") },
-            )
+            Row(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                Icon(
+                    Icons.Default.Clear,
+                    contentDescription = getString(MR.strings.clear_input),
+                    modifier = Modifier.clickable { onValueChange("") },
+                )
+                Box {
+                    var expanded by remember { mutableStateOf(false) }
+                    Icon(
+                        Icons.Default.Info,
+                        contentDescription = getString(MR.strings.clear_input),
+                        modifier = Modifier.clickable { expanded = true },
+                    )
+                    LanguageChooser(expanded, onDismissRequest = { expanded = false }, onLanguageSelected = {})
+                }
+            }
         }
     )
 }
--- shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchView.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchView.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
