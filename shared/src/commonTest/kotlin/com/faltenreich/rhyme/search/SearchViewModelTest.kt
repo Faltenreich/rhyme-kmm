cec05cc636e03d7707de5728b97f7501b6badb79
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-11-20 22:16:05 +0100

    Testing search view model

--- shared/src/commonTest/kotlin/com/faltenreich/rhyme/search/SearchViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/faltenreich/rhyme/search/SearchViewModelTest.kt
@@ -3,18 +3,41 @@ package com.faltenreich.rhyme.search
 import com.faltenreich.rhyme.shared.di.inject
 import com.faltenreich.rhyme.testModule
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
 class SearchViewModelTest: KoinTest {
 
-    @Test
-    fun `opens in idle state`() {
+    @BeforeTest
+    fun setup() {
         startKoin {
             modules(testModule())
         }
+    }
+
+    @AfterTest
+    fun clean() {
+        stopKoin()
+    }
+
+    @Test
+    fun `opens in idle state`() {
         val viewModel = inject<SearchViewModel>()
         assertEquals(SearchState.Idle, viewModel.uiState.value)
+    }
+
+    @Test
+    fun `reaches idle state on blank query`() {
+        val viewModel = inject<SearchViewModel>()
+        viewModel.onQueryChanged("")
+        assertEquals(SearchState.Idle, viewModel.uiState.value)
+    }
+
+    @Test
+    fun `reaches loading state on non-blank query`() {
+        val viewModel = inject<SearchViewModel>()
+        viewModel.onQueryChanged("Query")
+        assertTrue(viewModel.uiState.value is SearchState.Loading)
     }
 }
