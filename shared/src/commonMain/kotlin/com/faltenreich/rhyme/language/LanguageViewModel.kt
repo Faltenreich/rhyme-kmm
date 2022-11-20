b3ae5adbfbea2b59b21a789c751f3ea1b0dca86b
Philipp Fahlteich <philipp.fahlteich@gmail.com>
2022-11-20 22:02:36 +0100

    Fixing tests by injecting coroutine dispatcher into view model

--- shared/src/commonMain/kotlin/com/faltenreich/rhyme/language/LanguageViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/rhyme/language/LanguageViewModel.kt
@@ -1,20 +1,27 @@
 package com.faltenreich.rhyme.language
 
 import com.faltenreich.rhyme.shared.architecture.ViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
-class LanguageViewModel(private val repository: LanguageRepository): ViewModel() {
+class LanguageViewModel(
+    private val repository: LanguageRepository,
+    dispatcher: CoroutineDispatcher,
+): ViewModel() {
 
     private val state = MutableStateFlow(LanguageState(currentLanguage = Language.default))
     val uiState = state.asStateFlow()
 
     init {
-        repository.currentLanguage
-            .map { language -> state.value = state.value.copy(currentLanguage = language) }
-            .launchIn(viewModelScope)
+        viewModelScope.launch(dispatcher) {
+            repository.currentLanguage
+                .map { language -> state.value = state.value.copy(currentLanguage = language) }
+                .collect()
+        }
     }
 
     fun setCurrentLanguage(language: Language) {
--- shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchViewModel.kt
+++ shared/src/commonMain/kotlin/com/faltenreich/rhyme/search/SearchViewModel.kt
@@ -3,27 +3,31 @@
 package com.faltenreich.rhyme.search
 
 import com.faltenreich.rhyme.shared.architecture.ViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
 class SearchViewModel(
     private val searchUseCase: SearchUseCase,
+    dispatcher: CoroutineDispatcher,
 ): ViewModel() {
 
     private val state = MutableStateFlow<SearchState>(SearchState.Idle)
     val uiState = state.asStateFlow()
 
     init {
-        state
-            .filterIsInstance<SearchState.Loading>()
-            .debounce(1.seconds)
-            .distinctUntilChanged()
-            .flatMapLatest { state -> searchUseCase(state.query) }
-            .onEach { words -> state.value = SearchState.Result(state.value.query, words) }
-            // FIXME: Module with the Main dispatcher had failed to initialize. For tests Dispatchers.setMain from kotlinx-coroutines-test module can be used
-            .launchIn(viewModelScope)
+        viewModelScope.launch(dispatcher) {
+            state
+                .filterIsInstance<SearchState.Loading>()
+                .debounce(1.seconds)
+                .distinctUntilChanged()
+                .flatMapLatest { state -> searchUseCase(state.query) }
+                .onEach { words -> state.value = SearchState.Result(state.value.query, words) }
+                .collect()
+        }
     }
 
     fun onQueryChanged(query: String) {
